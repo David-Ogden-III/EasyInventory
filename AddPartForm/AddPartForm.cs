65c7bd1de979c7318451a8c06b0e41c7f8ec6cfe
David Ogden <david-ogden699@hotmail.com>
2024-05-20 22:41:28 -0500

    Add part form now accepts user input and adds part to inventory.AllParts

--- AddPartForm/AddPartForm.cs
+++ AddPartForm/AddPartForm.cs
@@ -9,11 +9,31 @@
 
         private void AddPartForm_Load(object sender, EventArgs e)
         {
-
+            string idPreview = Part.IdPreview().ToString();
+            AddPartIdInput.Text = idPreview;
         }
 
         private void AddPartSave_Click(object sender, EventArgs e)
         {
+            string partName = AddPartNameInput.Text;
+            int inventory = Convert.ToInt32(AddPartInventoryInput.Text);
+            decimal price = Convert.ToDecimal(AddPartPriceInput.Text);
+            int min = Convert.ToInt32(AddPartMinInput.Text);
+            int max = Convert.ToInt32(AddPartMaxInput.Text);
+            dynamic newPart;
+
+            if (OutsourcedRadio.Checked)
+            {
+                string compName = AddPartCompanyInput.Text;
+                newPart = new Outsourced(partName, price, inventory, min, max, compName);
+            }
+            else
+            {
+                int machineId = Convert.ToInt32(AddPartMachineIdInput.Text);
+                newPart = new InHouse(partName, price, inventory, min, max, machineId);
+            }
+            Inventory.AllParts.Add(newPart);
+
             var MainScreen = Tag as MainScreen;
             MainScreen?.Show();
             Close();
@@ -29,13 +49,10 @@
 
         private void OutsourcedRadio_CheckedChanged(object sender, EventArgs e)
         {
-            if (OutsourcedRadio.Checked)
-            {
                 AddPartMachineIdInput.Hide();
                 AddPartMachineIdLabel.Hide();
                 AddPartCompanyInput.Show();
                 AddPartCompanyNameLabel.Show();
-            }
         }
 
         private void InHouseRadio_CheckedChanged(object sender, EventArgs e)
