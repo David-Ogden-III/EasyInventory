c3c585091d587c8f79343c671dc4e3d472acda23
David Ogden <david-ogden699@hotmail.com>
2024-05-29 12:48:45 -0500

    fixed mock data entries

--- Program.cs
+++ Program.cs
@@ -11,7 +11,7 @@ static class Program
     static void Main()
     {
 
-        // Dummy Data
+        // Mock Data
         // Initialize Objects
         Product dirtBike = new("Dirt Bike", 3999.99M, 10, 3, 20);
         Product atv = new("ATV", 4999.99M, 8, 3, 20);
@@ -20,9 +20,9 @@ static class Program
         Outsourced wheel = new("Wheel", 499.99M, 3, 0, 5, "Dunlop");
         Outsourced brakes = new("Brakes", 129.99M, 1, 0, 10, "Shimona");
         Outsourced chain = new("Chain", 89.99M, 7, 5, 15, "EK");
-        InHouse fender = new("Fender", 369.99M, 8, 12, 20, 37634);
+        InHouse fender = new("Fender", 369.99M, 12, 8, 20, 37634);
         InHouse handlebars = new("Handlebars", 239.99M, 6, 5, 12, 15479);
-        InHouse suspension = new("Suspension", 799.99M, 6, 7, 8, 35964);
+        InHouse suspension = new("Suspension", 799.99M, 5, 3, 8, 35964);
 
         // Add Parts to Associated Parts lists
         dirtBike.AddAssociatedPart(fender);
